7274e049b1b10764d24cf94a9839930b2a7d7506
furtadoasaph <furtadoasaph@gmail.com>
2026-06-24 18:12:21 -0300

    Implement CourseService with CRUD operations

---
+++ Service/CourseService.java
@@ -0,0 +1,68 @@
+package com.ufpa.SAGUI.service;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ufpa.SAGUI.dto.course.CourseRequest;
+import com.ufpa.SAGUI.dto.course.CourseResponse;
+import com.ufpa.SAGUI.enums.EntityStatus;
+import com.ufpa.SAGUI.models.Course;
+import com.ufpa.SAGUI.repository.CourseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseResponse create(CourseRequest dto) {
+        Course course = Course.builder()
+                .name(dto.name())
+                .description(dto.description())
+                .build();
+        course.setStatus(EntityStatus.Active);
+        
+        return CourseResponse.from(courseRepository.save(course));
+    }
+
+    public CourseResponse update(UUID id, CourseRequest dto) {
+        Course course = getCourseEntity(id);
+        course.setName(dto.name());
+        course.setDescription(dto.description());
+        
+        return CourseResponse.from(courseRepository.save(course));
+    }
+
+    public void changeStatus(UUID id, EntityStatus status) {
+        Course course = getCourseEntity(id);
+        course.setStatus(status);
+        courseRepository.save(course);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CourseResponse> findAll(EntityStatus status, Pageable pageable) {
+        if (status != null) {
+            return courseRepository.findAllByStatus(status, pageable).map(CourseResponse::from);
+        }
+        return courseRepository.findAll(pageable).map(CourseResponse::from);
+    }
+
+    @Transactional(readOnly = true)
+    public CourseResponse findById(UUID id) {
+        return CourseResponse.from(getCourseEntity(id));
+    }
+
+    private Course getCourseEntity(UUID id) {
+        return courseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Curso não encontrado"));
+    }
+}
